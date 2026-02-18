f7c8db7251c25b9233d1f9d590d54cd5cb17a2ea
rajadurai <rajadurai1981@gmail.com>
2026-02-18 17:10:14 +0530

    Handling POST request

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 const app = express();
+
+//middleware
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   //res.status(200).send('Hello from the server side');
 //   res
@@ -15,6 +19,24 @@ app.get('/api/v1/tours', (req, res) => {
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
+
+// create new tour
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  //res.send('Successful');
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    },
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
