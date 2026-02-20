81ee093996018d31811ec7e82ba099ac2528f6a9
rajadurai <rajadurai1981@gmail.com>
2026-02-20 20:05:33 +0530

    static files and env variables

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1) MIDDLEWARES
+//console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgon('dev'));
+}
 
-app.use(morgon('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log('Hello from the middleware ;~)');
   next();
